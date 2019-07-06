6c59c120ded392fee0fdcbe4f9ba7da877a88d6c
elijahmm17 <elijahmm17@gmail.com>
2019-07-06 02:08:59 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,9 @@
 class Character < ActiveRecord::Base
   belongs_to :actor 
   belongs_to :show 
-end
 
-def say_that_thing_you_say 
-  self.catchphrase
-end 
+  def say_that_thing_you_say 
+    self.catchphrase
+  end 
+
+end
