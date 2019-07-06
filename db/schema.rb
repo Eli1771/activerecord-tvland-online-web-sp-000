acea56cca27f03445b3f7fefd7bbdfc28c9b72db
elijahmm17 <elijahmm17@gmail.com>
2019-07-06 02:05:41 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_015053) do
+ActiveRecord::Schema.define(version: 2019_07_06_014441) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2019_07_06_015053) do
     t.string "name"
     t.integer "show_id"
     t.string "catchphrase"
-    t.integer "actor_id"
   end
 
   create_table "networks", force: :cascade do |t|
